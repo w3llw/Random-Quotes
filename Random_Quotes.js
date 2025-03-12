fca9c58bf2045f3672b10589cdde4d4447095baf
w3llw <imv05@inbox.ru>
2025-03-12 18:32:55 +0300

    Изменен URL API для получения случайной цитаты с HTTPS на HTTP

--- Random_Quotes.js
+++ Random_Quotes.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Получение случайной цитаты из API Quotable
     const fetchQuote = async () => {
         try {
-            const response = await fetch('https://api.quotable.io/random');
+            const response = await fetch('http://api.quotable.io/random');
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
